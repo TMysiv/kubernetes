e619dd08f855ffc029effcb065948661aaa49791
Taras Mysiv <taras.mysiv@chisw.com>
2023-03-16 13:49:14 +0200

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    console.log(Math.floor(Math.random()))
+    return this.appService.getHello();
+  }
+}
